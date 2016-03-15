4d530017458af4694845108a2faec3058403ed5c
koallen <weifengzi2009@gmail.com>
2016-03-15 15:16:57 +0800

    trying to add app bar and search feature

--- app/src/main/java/sg/edu/ntu/cz2006/seproject/MainActivity.java
+++ app/src/main/java/sg/edu/ntu/cz2006/seproject/MainActivity.java
@@ -1,9 +1,17 @@
 package sg.edu.ntu.cz2006.seproject;
 
+import android.app.SearchManager;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
@@ -21,6 +29,9 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         MapFragment mapFragment =
                 (MapFragment) getFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.tool_bar);
+        setSupportActionBar(myToolbar);
     }
 
     @Override
@@ -28,11 +39,43 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         map.addMarker(new MarkerOptions()
                 .position(new LatLng(1.3, 103.8))
                 .title("Marker"));
-        map.setMyLocationEnabled(true);
+        //map.setMyLocationEnabled(true);
     }
 
     public void fabOnClickListener(View view) {
         Intent nav = new Intent(this, NavigationActivity.class);
         startActivity(nav);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        SearchManager searchManager =
+                (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView =
+                (SearchView) menu.findItem(R.id.action_search).getActionView();
+        searchView.setSearchableInfo(
+                searchManager.getSearchableInfo(new ComponentName(this, NavigationActivity.class)));
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        } else if (id == R.id.action_search) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/sg/edu/ntu/cz2006/seproject/NavigationActivity.java
+++ app/src/main/java/sg/edu/ntu/cz2006/seproject/NavigationActivity.java
@@ -1,11 +1,11 @@
 package sg.edu.ntu.cz2006.seproject;
 
+import android.app.SearchManager;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
 
 public class NavigationActivity extends AppCompatActivity {
 
@@ -13,6 +13,22 @@ public class NavigationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation);
+        handleIntent(getIntent());
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        handleIntent(intent);
+    }
+
+    private void handleIntent(Intent intent) {
+
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            //use the query to search your data somehow
+            TextView text = (TextView) findViewById(R.id.textview);
+            text.setText(query);
+        }
     }
 
     public void buttonOnClickListener(View view) {
