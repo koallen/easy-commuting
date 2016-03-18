c02cc1d694144eec083faf6adec098b691cec899
koallen <weifengzi2009@gmail.com>
2016-03-19 02:02:42 +0800

    add anchored FAB to NavigationActivity

--- app/src/main/java/sg/edu/ntu/cz2006/seproject/NavigationActivity.java
+++ app/src/main/java/sg/edu/ntu/cz2006/seproject/NavigationActivity.java
@@ -1,15 +1,23 @@
 package sg.edu.ntu.cz2006.seproject;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.location.Location;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.maps.android.PolyUtil;
@@ -19,11 +27,17 @@ import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class NavigationActivity extends AppCompatActivity implements OnMapReadyCallback {
     @Bind(R.id.nav_rv)
     RecyclerView recyclerView;
+    @Bind(R.id.fab)
+    FloatingActionButton mFab;
     private String apiData;
+    private GoogleApiClient mGoogleApiClient;
+    private Location mLastLocation;
+    private GoogleMap mMap;
     private static final LatLng SINGAPORE = new LatLng(1.3, 103.8);
 
     @Override
@@ -59,10 +73,44 @@ public class NavigationActivity extends AppCompatActivity implements OnMapReadyC
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
         List<LatLng> waypoints = PolyUtil.decode(apiData);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        googleMap.setMyLocationEnabled(true);
         googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(SINGAPORE, 10));
         googleMap.addPolyline(new PolylineOptions()
                 .addAll(waypoints)
                 .color(Color.BLUE));
+    }
+
+    @OnClick(R.id.fab)
+    public void onFabClicked() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+                mGoogleApiClient);
+        LatLng myLocation = new LatLng(mLastLocation.getLatitude(), mLastLocation.getLongitude());
+        locateUser(myLocation);
+    }
+
+    public void locateUser(LatLng userLocation) {
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(userLocation, 15));
     }
 }
