6cbc8266d0b85b9d7ab15e9bb4d3821a3da34f95
koallen <weifengzi2009@gmail.com>
2016-04-10 15:12:47 +0800

    add a release apk

--- app/src/test/java/sg/edu/ntu/cz2006/seproject/SuggestionGenerationHelperUnitTest.java
+++ app/src/test/java/sg/edu/ntu/cz2006/seproject/SuggestionGenerationHelperUnitTest.java
@@ -1,13 +1,16 @@
 package sg.edu.ntu.cz2006.seproject;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
 
 import static org.junit.Assert.*;
 
 /**
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
  */
-public class ExampleUnitTest {
+@RunWith(MockitoJUnitRunner.class)
+public class SuggestionGenerationHelperUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
